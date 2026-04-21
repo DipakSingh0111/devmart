f6a59b0995fc15265d9cae2fd34c759345176022
Dipak Singh <dipaksingh3323@gmail.com>
2026-04-21 22:05:05 +0530

    update apiData with localhost url to live vercel backend url

--- src/utils/apiData.js
+++ src/utils/apiData.js
@@ -3,7 +3,7 @@ export const API_MAP = {
   latest: "https://dummyjson.com/products?skip=0&limit=10",
   women: "https://dummyjson.com/products/category/womens-dresses",
   men: "https://dummyjson.com/products/category/mens-shirts",
-  kids:"https://dummyjson.com/products/category/tops",
+  kids: "https://dummyjson.com/products/category/tops",
   singleProduct: "https://dummyjson.com/products",
-  auth:"http://localhost:8080"
+  auth: "https://devmart-backend.vercel.app/",
 };
